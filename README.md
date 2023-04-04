3f6ec8e214edb06e0a40458d8130bed24b9084f1
Chandler McWilliams <chandler@brysonian.com>
2023-04-04 11:25:50 +0900

    move config to .env

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 PHP and Markdown based website builder for DMA classes
 
-`runner.php` has a few PHP-level config options, all of which are optional.
-* defining `DEBUG` will enable error display (this may be affected by local php config)
-* defining `USE_PHAR` will load the code from the app dir rather than the courseware.phar
-* defining `CONTENT_DIR` will use the specified directory to load site content. The default is `content`
+To set some PHP-level config options, rename .env.example to .env
+all of these are optional and the default values are described below
+
+* `DEBUG` enables error display (this may be affected by local php config)
+* `USE_PHAR` loads the app code from the specified phar file rather than the `app` directory
+* `CONTENT_DIR` will use the specified directory to load site content. The default is `content`
 
 ## Building sass
 Styles are written in sass. You can compile them by cding into `app` via parcel using `npm run build` You can also watch for changes (useful for development) using `npm start`
--- runner.php
+++ runner.php
@@ -1,36 +1,33 @@
 <?php
 
-##########################################
-#             CONFIGURATION              #
-##########################################
+# Global config for this install can be set by creating a .env file
 
-// define('USE_PHAR', 1);
-
-# use the specified directory to load site content, the default is to use the dir named `content`
-define('CONTENT_DIR', '152');
-
-# enable error display (this may be affected by local php config)
-# define('DEBUG', 1);
-
-
+if (file_exists('.env')) {
+  $env = parse_ini_file('.env');
+  foreach ($env as $k => $v) {
+    define($k, $v);
+  }
+}
 
 ##########################################
 define('PUBLIC_ROOT', __DIR__);
 
 # open htaccess and parse out base path
-$base = '';
-try {
-  $htaccess = file_get_contents(realpath(PUBLIC_ROOT . '/.htaccess'));
-  $m = [];
-  preg_match_all('/RewriteBase(.*)/', $htaccess, $m);
-  $base = trim($m[1][0]);
-  $base = $base === '/' ? '' : $base;
-} catch(Exception $e) {}
+if (!defined('PUBLIC_URL')) {
+  $base = '';
+  try {
+    $htaccess = file_get_contents(realpath(PUBLIC_ROOT . '/.htaccess'));
+    $m = [];
+    preg_match_all('/RewriteBase(.*)/', $htaccess, $m);
+    $base = trim($m[1][0]);
+    $base = $base === '/' ? '' : $base;
+  } catch(Exception $e) {}
 
-define('PUBLIC_URL', $base);
+  define('PUBLIC_URL', $base);
+}
 
 if (defined('USE_PHAR')) {
-  require './courseware.phar';
+  require USE_PHAR;
 } else {
   require './app/app.php';
 }
